09c114df147c2fa4a15951faf75fae25da53760b
Mario Angelino <mario.angelino@hotmail.com>
2025-09-16 05:54:25 -0300

    atualizar kpi

--- kpi.py
+++ kpi.py
@@ -2,4 +2,7 @@
 
 nome = input("Digite o seu nome: ")
 valor_salario = float(input("Digite o valor do seu salário mensal: "))
-valor_bonus = float(input("Digite o valor do bônus recebido: "))
+percentual_bonus = float(input("Digite o percentual do bônus recebido: "))
+
+bonus_recebido = ((valor_salario * percentual_bonus) + 1000)
+print(f"Olá {nome}, seu bônus recebido foi: {bonus_recebido:.2F}.")
